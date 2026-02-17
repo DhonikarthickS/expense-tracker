9990808c2a9dfd09f10c16eda6d531e9bc2e8a4a
DHonikarthikS <dhonikarthicksalem@gmail.com>
2026-02-17 22:30:45 +0530

    db mongoose change for buffer

---
+++ server/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) return;
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("Mongo error:", err);
+    throw err;
+  }
+};
+
+module.exports = connectDB;
--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const connectDB = require("./db");
+
 const financialRoutes = require("./routes/financial-records");
 
 const app = express();
@@ -10,10 +12,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("Mongo error:", err));
+app.use(async (req, res, next) => {
+  try {
+    await connectDB();
+    next();
+  } catch (err) {
+    res.status(500).json({ error: "Database connection error" });
+  }
+});
 
 app.use("/api/financial-records", financialRoutes);
 app.use("/financial-records", financialRoutes);
